3c06bf4d1297148ef69c383d4766056cbeb24f67
proy27 <proy55663000@gmail.com>
2020-02-21 13:58:12 +0800

    Add files via upload

---
+++ WideFB.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         WideFB
+// @namespace    https://github.com/proy27
+// @version      0.1
+// @description  
+// @author       Proy27
+// @match        https://www.facebook.com/*
+// @grant        none
+// @require http://code.jquery.com/jquery-3.4.1.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    $("#ssrb_composer_start").parent().css("width","80%");
+
+
+})();
